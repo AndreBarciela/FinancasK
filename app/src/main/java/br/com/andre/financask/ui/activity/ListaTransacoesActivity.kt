4fa3f3ed38271a4e3035269c0bf18e861e46f9ce
AndreBarciela <barciela@bpepper.com.br>
2019-01-22 17:03:31 -0200

    adicionando valor de receita na activity

--- app/src/main/java/br/com/andre/financask/ui/activity/ListaTransacoesActivity.kt
+++ app/src/main/java/br/com/andre/financask/ui/activity/ListaTransacoesActivity.kt
@@ -4,10 +4,12 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.widget.ArrayAdapter
 import br.com.andre.financask.R
+import br.com.andre.financask.extension.formataParaBrasileiro
 import br.com.andre.financask.model.Tipo
 import br.com.andre.financask.model.Transacao
 import br.com.andre.financask.ui.adapter.ListaTransacoesAdapter
 import kotlinx.android.synthetic.main.activity_lista_transacoes.*
+import kotlinx.android.synthetic.main.resumo_card.*
 import java.math.BigDecimal
 import java.util.*
 
@@ -17,13 +19,34 @@ class ListaTransacoesActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lista_transacoes)
 
-        val transacoes = listOf(
+        val transacoes: List<Transacao> = transacoesDeExemplo()
+
+        adicionaReceitaNoResumo(transacoes)
+
+        configuraLista(transacoes)
+    }
+
+    private fun adicionaReceitaNoResumo(transacoes: List<Transacao>) {
+        var totalReceita = BigDecimal.ZERO
+        for (transacao in transacoes) {
+            if (transacao.tipo == Tipo.RECEITA) {
+                totalReceita = totalReceita.plus(transacao.valor)
+            }
+        }
+
+        resumo_card_receita.text = totalReceita.formataParaBrasileiro()
+    }
+
+    private fun configuraLista(transacoes: List<Transacao>) {
+        lista_transacoes_listview.adapter = ListaTransacoesAdapter(transacoes, this)
+    }
+
+    private fun transacoesDeExemplo(): List<Transacao> {
+        return listOf(
             Transacao(valor = BigDecimal(20.5), tipo = Tipo.DESPESA),
             Transacao(valor = BigDecimal(100), categoria = "Economia", tipo = Tipo.RECEITA),
             Transacao(valor = BigDecimal(200.0), tipo = Tipo.DESPESA),
             Transacao(valor = BigDecimal(500.0), categoria = "Prêmio", tipo = Tipo.RECEITA)
         )
-
-        lista_transacoes_listview.adapter = ListaTransacoesAdapter(transacoes, this)
     }
 }
